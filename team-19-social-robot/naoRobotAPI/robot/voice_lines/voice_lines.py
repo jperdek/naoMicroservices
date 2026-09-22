595a598f6f3bc080460234208392b33ecbfdabd3
Jakub Perdek <perdek.jakub@gmail.com>
2026-09-22 16:24:24 +0200

    Fixing speed range issue and float convertion for wait issue

--- team-19-social-robot/naoRobotAPI/robot/voice_lines/voice_lines.py
+++ team-19-social-robot/naoRobotAPI/robot/voice_lines/voice_lines.py
@@ -9,14 +9,18 @@ import string
 if os.environ.get("LOCAL", "True") == "True":
     dotenv.load_dotenv()
 
-    
+def set_to_nao_speed_range(current_speed):
+    if 0 > int(current_speed) > 100:
+        raise Exception("Exceeded allowed limits")
+    return int(int(current_speed) * 350 / 100 + 50)
+
 def set_voice_lines(voice_lines_config):
     if "speech_proxy" not in g:
         NAO_IP = os.environ.get("NAO_IP", "127.0.0.1")
         NAO_PORT = int(os.environ.get("NAO_PORT", 9559))
         g.speech_proxy = ALProxy("ALTextToSpeech", NAO_IP, NAO_PORT)
     if "speed" in voice_lines_config:
-        g.speech_proxy.setParameter("speed", int(voice_lines_config["speed"]))
+        g.speech_proxy.setParameter("speed", int(set_to_nao_speed_range(voice_lines_config["speed"])))
     elif "lang" not in voice_lines_config or voice_lines_config["lang"] == "English":
         g.speech_proxy.setParameter("speed", 80)
     elif voice_lines_config["lang"] == "sk":
@@ -27,7 +31,7 @@ def set_voice_lines(voice_lines_config):
     if "voice_lines_configs" in voice_lines_config:
         for _, voice_line_config in voice_lines_config["voice_lines_configs"].items():
             if "wait" in voice_line_config and voice_line_config["wait"]:
-                time.sleep(voice_line_config["wait"])
+                time.sleep(float(voice_line_config["wait"]))
             text = unicode(voice_line_config["translation"]).encode('utf8', 'replace')
             try:
                 g.speech_proxy.say("" + text, lang)
